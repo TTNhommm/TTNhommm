fb970695e3181417562330332c4fdd6472564936
meothang <36185478+meothang@users.noreply.github.com>
2020-05-06 23:36:01 +0700

    update

--- resources/views/backend/product/product.blade.php
+++ resources/views/backend/product/product.blade.php
@@ -15,15 +15,50 @@
         <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <div class="page-head-text">
-                        <h1 class="panel-title"><strong>Quản lý</strong> sản phẩm</h1>
-                        <a href="{{ route('admin.get.create.product')}}">
-                            <button class="btn btn-primary btn-rounded pull-right"><span class="fa fa-plus"></span> Thêm
-                                sản phẩm</button>
-                        </a>
+                    <div class="row">
+                        <div class="page-head-text col-md-6">
+                            <h1 class="panel-title"><strong>Quản lý</strong> sản phẩm</h1>
+                        </div>
+                        <div class="page-head-controls col-md-6">
+                            <div class="row">
+                                <div class="col-md-9">
+                                    <form action="">
+                                        <div class="input-group" style="display: inline-flex;">
+                                            <span class="input-group-addon"><span class="fa fa-search"></span></span>
+                                            <input name="name" type="text" size="30" class="form-control"
+                                                placeholder="Vui lòng nhập sản phẩm cần tìm kiếm"
+                                                value="{{ \Request::get('name') }}">
+                                            <div class="form-group" style="margin:0px">
+                                                <select class="form-control select" name="cate" id="">
+                                                    <option>Danh mục</option>
+                                                    @if($categories)
+                                                    @foreach($categories as $item)
+                                                    <option value="{{ $item->id }}"
+                                                        {{ \Request::get('cate') == $item->id ? "selected = 'selected'" : ""  }}>
+                                                        {{ $item->name }}
+                                                    </option>
+                                                    @endforeach
+                                                    @endif
+                                                </select>
+                                            </div>
+                                            <div class="input-group-btn">
+                                                <button type="submit" class="btn btn-primary">Search</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="col-md-3">
+                                    <a href="{{ route('admin.get.create.product')}}">
+                                        <button class="btn btn-primary btn-rounded pull-right" style="width:100%"><span
+                                                class="fa fa-plus"></span> Thêm
+                                            sản phẩm</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="row">
+                {{-- <div class="row">
                     <div class="col-md-6">
                         <form action="" class="form-inline" style="margin-bottom :15px">
                             <div class="form-group">
@@ -47,7 +82,8 @@
                             <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
                         </form>
                     </div>
-                </div>
+                </div> --}}
+
                 <div class="panel-body panel-body-table">
                     <div class="table-responsive">
                         <table class="table table-bordered table-striped table-actions">
@@ -76,8 +112,8 @@
                                         hidden; -webkit-box-orient: vertical;border-width:1px 0 0 0">
                                         {{$product->pro_content}}</td>
                                     <td class="text-center"><img class="img-fluid" style="width:100px"
-                                            src="{{ asset("img/product/$product->pro_image")}}" alt=""></td>
-                                    <!--  src="{{asset("public/img/product/$product->pro_image")}}" alt=""></td> -->
+                                            {{-- src="{{ asset("img/product/$product->pro_image")}}" alt=""></td> --}}
+                                    src="{{asset("public/img/product/$product->pro_image")}}" alt=""></td>
                                     <td class="text-center">{{ $product->pro_type}}</td>
                                     <td class="text-center">{{ number_format($product->pro_price) }} VNĐ</td>
                                     <?php
